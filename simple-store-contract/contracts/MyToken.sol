9f0cfcd32d225e26c72b3ee32c5ce10d6525ec2e
ju4ncito <villarrealjm2@gmail.com>
2025-11-26 20:34:00 -0300

    created own contract. testing

---
+++ simple-store-contract/contracts/MyToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+contract MyToken {
+    string public name;
+    string public symbol;
+
+    uint8 public decimals;
+    uint256 public totalSupply;
+}
